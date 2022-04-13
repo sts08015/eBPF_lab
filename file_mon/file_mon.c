a5ac83e3447830697dea044fe9816a72f3fc6bd5
sts08015 <sts08015@gmail.com>
2022-04-13 21:43:25 +0900

    phew

--- file_mon/file_mon.c
+++ file_mon/file_mon.c
@@ -5,12 +5,15 @@
 
 struct data_t {
     char name[TASK_COMM_LEN];
-    u64 read_cnt;
-    u64 write_cnt;
     umode_t type;
 };
 
-BPF_ARRAY(arr,struct data_t);
+struct count {
+    u64 read_cnt;
+    u64 write_cnt;
+};
+
+BPF_HASH(table,struct data_t, struct count);
 
 static void get_name(void* buf,struct file* file, u64 size)
 {
@@ -24,12 +27,36 @@ static void get_type(umode_t* buf,struct file* file)
     *buf = file->f_inode->i_mode;
 }
 
+static void inc_read(struct data_t* data)
+{
+    struct count* tmp = table.lookup(data);
+    if(tmp == NULL)
+    {
+        struct count cnt = {0};
+        ++(cnt.read_cnt);
+        table.insert(data,&cnt);
+    }
+    else ++(tmp->read_cnt);
+}
+
+static void inc_write(struct data_t* data)
+{
+    struct count* tmp = table.lookup(data);
+    if(tmp == NULL)
+    {
+        struct count cnt = {0};
+        ++(cnt.write_cnt);
+        table.insert(data,&cnt);
+    }
+    else ++(tmp->write_cnt);
+}
+
 ssize_t read_mon(struct pt_regs *ctx,struct file *file)
 {
     struct data_t data = {0};
     get_name(&data.name,file,TASK_COMM_LEN);
     get_type(&data.type,file);
-    //look up --> inc readcnt
+    inc_read(&data);
     return 0;
 }
 
@@ -38,7 +65,7 @@ ssize_t readv_mon(struct pt_regs *ctx,struct file *file)
     struct data_t data = {0};
     get_name(&data.name,file,TASK_COMM_LEN);
     get_type(&data.type,file);
-
+    inc_read(&data);
     return 0;
 }
 
@@ -47,7 +74,7 @@ ssize_t write_mon(struct pt_regs *ctx,struct file *file)
     struct data_t data = {0};
     get_name(&data.name,file,TASK_COMM_LEN);
     get_type(&data.type,file);
-
+    inc_write(&data);
     return 0;
 }
 
@@ -56,6 +83,6 @@ ssize_t writev_mon(struct pt_regs *ctx,struct file *file)
     struct data_t data = {0};
     get_name(&data.name,file,TASK_COMM_LEN);
     get_type(&data.type,file);
-
+    inc_write(&data);
     return 0;
 }
--- file_mon/file_mon.py
+++ file_mon/file_mon.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 from bcc import BPF
 from time import sleep
+from stat import *
 
 b = BPF(src_file='./file_mon.c')
 b.attach_kprobe(event='vfs_read',fn_name='read_mon')
@@ -18,7 +19,40 @@ try:
 except KeyboardInterrupt:
     pass
 
-table = b.get_table('arr')
-#shoud parse file type here.
-#https://docs.python.org/ko/3/library/stat.html
+table = b.get_table('table')
+
+print("\n%-20s %-25s %15s:%-15s %15s:%-15s" %("NAME","TYPE","READ","NUM","WRITE","NUM"))
+
+for i in table.items():
+    key = i[0]
+    val = i[1]
+    name = key.name
+    mode = key.type
+    read = val.read_cnt
+    write = val.write_cnt
+    if S_ISDIR(mode):
+        mode = "directory"
+    elif S_ISCHR(mode):
+        mode = "character special dev"
+    elif S_ISBLK(mode):
+        mode = "block special dev"
+    elif S_ISREG(mode):
+        mode = "regular file"
+    elif S_ISFIFO(mode):
+        mode = "FIFO"
+    elif S_ISLNK(mode):
+        mode = "symbolic link"
+    elif S_ISSOCK(mode):
+        mode = "socket"
+    elif S_ISDOOR(mode):
+        mode = "door"
+    elif S_ISPORT(mode):
+        mode = "event port"
+    elif S_ISWHT(mode):
+        mode = "whiteout"
+    else:
+        mode = '?'
+    
+    print("\n%-20s %-25s %15s:%-15s %15s:%-15s" %(name,mode,"READ",read,"WRITE",write))
+    
 
